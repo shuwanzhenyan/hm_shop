f29268798fee89af47b137c6a734223166d328bf
GreatSpirit <great.spirit@163.com>
2026-08-24 14:29:06 +0800

    完成轮播图的搜索框和指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -12,11 +12,16 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  CarouselSliderController _controller = CarouselSliderController();
+  int _currentIndex = 0; // 当前激活的索引
+
+  // 获取轮播图
   Widget _getSlider() {
     // 获取屏幕宽度
     final double screenWidth = MediaQuery.of(context).size.width;
     // 第三方轮播图插件CarouselSlider
     return CarouselSlider(
+      carouselController: _controller, // CarouselSlider的controller
       items: List.generate(widget.bannerList.length, (int index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
@@ -29,19 +34,75 @@ class _HmSliderState extends State<HmSlider> {
         height: 300,
         viewportFraction: 1,
         autoPlay: true,
+        onPageChanged: (int index, CarouselPageChangedReason reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  // 获取搜索栏
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: 50,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text(
+            "搜索..",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: index == _currentIndex ? 40 : 20,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: index == _currentIndex
+                      ? Colors.white
+                      : Color.fromRGBO(0, 0, 0, 0.3),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_getSlider()]);
-    // return Container(
-    //   height: 300,
-    //   color: Colors.blue,
-    //   alignment: Alignment.center,
-    //   child: Text(
-    //       "轮播图", style: TextStyle(color: Colors.white, fontSize: 20)),
-    // );
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
