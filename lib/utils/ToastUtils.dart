c002502b110d00d78b2a55d21cbb8d3ef463cf8e
GreatSpirit <great.spirit@163.com>
2026-08-25 15:51:29 +0800

    登录的搭建和表单校验

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Toastutils {
+  static bool showLoading = false;
+
   static void showToast(BuildContext context, String? msg) {
+    if (Toastutils.showLoading) {
+      return;
+    }
+    Toastutils.showLoading = true;
+    Future.delayed(Duration(seconds: 3), () {
+      Toastutils.showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 120,
+        width: 180,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(40),
         ),
